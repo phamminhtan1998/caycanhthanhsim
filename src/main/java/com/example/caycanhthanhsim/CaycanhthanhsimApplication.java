d885e77f554997d5c6e6a1e5363bbf3654f0b28c
phamminhtan <phamtan1ltt1998@gmail.com>
2020-12-29 15:30:06 +0700

    init project

---
+++ src/main/java/com/example/caycanhthanhsim/CaycanhthanhsimApplication.java
@@ -0,0 +1,13 @@
+package com.example.caycanhthanhsim;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CaycanhthanhsimApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CaycanhthanhsimApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/caycanhthanhsim/CaycanhthanhsimApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.caycanhthanhsim;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CaycanhthanhsimApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
